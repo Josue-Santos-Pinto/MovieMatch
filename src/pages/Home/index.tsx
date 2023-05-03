c9f596e64f51923f742843308171dc32fbe09f67
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-05-03 11:17:42 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Texto } from './styles';
+
+export function Home() {
+  return (
+    <Container>
+      <Texto>Home</Texto>
+    </Container>
+  );
+}
