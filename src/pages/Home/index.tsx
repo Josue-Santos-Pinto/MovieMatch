5d02ef6d4a404385490608a2c17796fd91324372
Josue-Santos-Pinto <js.pinto55@gmail.com>
2023-05-08 18:09:00 -0300

    fix genre pick bug

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,6 +21,7 @@ import { FooterList } from '../../components/FooterList';
 export function Home() {
   const [list, setList] = useState<Movie[]>([]);
   const [genresList, setGenresList] = useState<Genre[]>([]);
+  const [currentGenre, setCurrentGenre] = useState(28);
   const [page, setPage] = useState(1);
   const [hasMoreData, setHasMoreData] = useState(true);
   const [loading, setLoading] = useState(false);
@@ -39,8 +40,8 @@ export function Home() {
       return;
     } else {
       setLoading(true);
-      const response = await api.getTopMovies(page);
 
+      const response = await api.getGenresMoviesList(currentGenre, page + 1);
       setList([...list, ...response.results]);
       setPage(page + 1);
       setLoading(false);
@@ -49,6 +50,7 @@ export function Home() {
   const getMoviesList = async (id: number) => {
     let res = await api.getGenresMoviesList(id, page);
     setList(res.results);
+    setCurrentGenre(id);
   };
   useEffect(() => {
     loadApi();
@@ -75,7 +77,7 @@ export function Home() {
         <Scroller horizontal showsHorizontalScrollIndicator={false}>
           {genresList != undefined &&
             genresList.map((item, index) => (
-              <GenresButton onPress={() => getMoviesList(item.id)}>
+              <GenresButton key={item.id} onPress={() => getMoviesList(item.id)}>
                 <GenresText>{item.name}</GenresText>
               </GenresButton>
             ))}
@@ -87,7 +89,6 @@ export function Home() {
           renderItem={({ item, index }) => <ListItem data={item} isLast={index === length - 1} />}
           keyExtractor={(item) => item.id.toString()}
           numColumns={2}
-          showsVerticalScrollIndicator={false}
           onEndReached={loadApi}
           onEndReachedThreshold={0.1}
           ListFooterComponent={<FooterList load={loading} />}
